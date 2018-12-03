6b2847385cb5d663f1ec22ad6eaa81b38bf29bd0
MARTINS RIBEIRO Mathieu <m.martinsribeiro@ipi.igsgroupe.com>
2018-12-03 10:15:50 +0100

    SpringBoot Project import
    SpringBoot Project import

---
+++ instant-message/src/main/java/com/im/instant/message/InstantMessage.java
@@ -0,0 +1,12 @@
+package com.im.instant.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstantMessage {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstantMessage.class, args);
+	}
+}
